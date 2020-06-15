86d66251b16ac7e54a969de17831a947cda2a622
Gabriel <alexandrugabrielnita@gmail.com>
2020-06-15 21:22:28 +0300

    2nd

--- M01S04/ex08/app.js
+++ M01S04/ex08/app.js
@@ -18,12 +18,22 @@ var i = 1;
 while (i < 67) {
   console.log(i);
 
-  if (i === 12){
+  if (i === 12) {
     break;
   }
 
   i++;
 }
 
-console.warn('Folosind keywordul continue afiseaza doar numerele pare intre 1 si 32 (inclusiv )');
+console.warn('Folosind keywordul continue afiseaza doar numerele pare intre 1 si 32 (inclusiv).');
 
+var i = 1;
+while (i <= 32) {
+  console.log(i);
+
+  if (i % 2 === 0) {
+    continue;
+  }
+  
+  i++;
+}
