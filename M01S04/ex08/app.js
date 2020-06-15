32af1e40b4684481f7add6947cc152323250ce07
Gabriel <alexandrugabrielnita@gmail.com>
2020-06-15 21:12:16 +0300

    first commit

--- M01S04/ex08/app.js
+++ M01S04/ex08/app.js
@@ -1,7 +1,29 @@
 var i = 1;
 
-while (i <= 100) {
+while (i <= 67) {
   console.log(i);
 
   i++;
 }
+
+
+while (i < 67) {
+  console.log(i);
+
+  i++;
+}
+
+console.warn('Folosind keywordul break opreste bucla atunci cand numarul este egal cu 12 (dar afiseaza-l).');
+var i = 1;
+while (i < 67) {
+  console.log(i);
+
+  if (i === 12){
+    break;
+  }
+
+  i++;
+}
+
+console.warn('Folosind keywordul continue afiseaza doar numerele pare intre 1 si 32 (inclusiv )');
+
