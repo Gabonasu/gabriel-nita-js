1b466fa565c7d4df0411cbca9fde91c380d95f40
Gabriel <alexandrugabrielnita@gmail.com>
2020-06-23 22:19:17 +0300

    x

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,5 +1,25 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(() => {
+  let showMessage = (message) => {
+    let messageParagraph = document.createElement('p');
+
+    messageParagraph.innerText = message;
+    messageParagraph.classList.add('message');
+
+    document.body.appendChild(messageParagraph);
+
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    let stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', () => {
+      console.log('Mouse-ul este pe scena');
+      showMessage('Mouse-ul este pe scena');
+    });
+
+    stage.addEventListener('mouseout', () => {
+      console.log('Mouse-ul nu este pe scena');
+      showMessage('Mouse-ul nu este pe scena');
+    });
   });
 })();
