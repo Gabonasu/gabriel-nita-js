286d57150dd99ae88f3695ccb7a221eede09b719
Gabriel <alexandrugabrielnita@gmail.com>
2020-06-23 18:54:19 +0300

    preFinalCommit

--- M01S06/ex01/app.js
+++ M01S06/ex01/app.js
@@ -3,6 +3,8 @@ let car = {
   color: 'black',
   wheels: 4,
   speed: 50,
+  topSpeed: 160,
+  topReverseSpeed: -50,
   isTrunkOpen: false,
   areLightsOn: false,
   accelerate: function () {
@@ -29,5 +31,26 @@ let car = {
     window.setTimeout(() => {
       this.turnLightsOff
     }, 3000);
+  },
+  stop: function () {
+    this.speed = 0;
+  },
+  setSpeed: function () {
+    let x = 0;
+    this.speed = x;
+
+    if (x >= this.topReverseSpeed) {
+      return x;
+    } else if (x <= this.topSpeed) {
+      return x;
+    } else {
+      stop ();
+    }
   }
 };
+
+console.warn('Afiseaza propozitia: "Masina era marca make si se deplasa cu speed km/h".');
+console.log(`Masina era marca ${car.make} si se deplasa cu ${car.speed} km/h.`);
+
+console.warn('Decelereaza masina cu 5 unitati apoi afisaza propozitia: "Viteza noua este speed km/h".');
+// ??? //
